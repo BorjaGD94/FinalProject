39535c38f6f6cc790914f72ec17403b7e6f10f8e
Borja Gonzalez Diaz <borjagonzalezdiaz94@gmail.com>
2018-11-20 16:04:43 -0600

    services working!!

---
+++ app/src/main/java/com/example/borja/finalproject/ExpandableListAdapter.java
@@ -0,0 +1,226 @@
+package com.example.borja.finalproject;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+
+//For expandable list view use BaseExpandableListAdapter
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+    private Context _context;
+    private List<String> header; // header titles
+    // Child data in format of header title, child title
+    private HashMap<String, List<Service>> child;
+
+    public ExpandableListAdapter(Context context, List<String> listDataHeader,
+                                 HashMap<String, List<Service>> listChildData) {
+        this._context = context;
+        this.header = listDataHeader;
+        this.child = listChildData;
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosititon) {
+
+        // This will return the child
+        return this.child.get(this.header.get(groupPosition)).get(
+                childPosititon);
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, final int childPosition,
+                             boolean isLastChild, View convertView, ViewGroup parent) {
+
+        // Getting child text
+        //final String childText = (String) getChild(groupPosition, childPosition);
+        //Service service = new Service("",0,"","");
+        //String childText = service.getServiceName();
+        final Service childText = (Service) getChild(groupPosition, childPosition);
+
+        // Inflating child layout and setting textview
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.childs, parent, false);
+        }
+        TextView child_name = (TextView) convertView.findViewById(R.id.serviceName);
+        TextView child_price = (TextView) convertView.findViewById(R.id.priceEstimate);
+        TextView child_time = (TextView) convertView.findViewById(R.id.waitingTime);
+        ImageView child_logo= (ImageView) convertView.findViewById(R.id.serviceIcon);
+        child_name.setText(childText.serviceName);
+        child_price.setText(childText.servicePrice);
+        child_time.setText(childText.servicePrice);
+        child_logo.setImageResource(childText.serviceLogo);
+
+        //child_text.setText(childText);
+        return convertView;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+
+        // return children count
+        return this.child.get(this.header.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+
+        // Get header position
+        return this.header.get(groupPosition);
+    }
+
+    @Override
+    public int getGroupCount() {
+
+        // Get header size
+        return this.header.size();
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent) {
+
+        // Getting header title
+        String headerTitle = (String) getGroup(groupPosition);
+
+        // Inflating header layout and setting text
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.header, parent, false);
+        }
+
+        TextView header_text = (TextView) convertView.findViewById(R.id.header);
+        if (groupPosition == 0) {
+            header_text.setBackground(ContextCompat.getDrawable(_context, R.drawable.spinner_layout));
+        }
+        if (groupPosition == 1) {
+            header_text.setBackground(ContextCompat.getDrawable(_context, R.drawable.spinner_layout1));
+        }
+        if (groupPosition == 2) {
+            header_text.setBackground(ContextCompat.getDrawable(_context, R.drawable.spinner_layout2));
+        }
+        if (groupPosition == 3) {
+            header_text.setBackground(ContextCompat.getDrawable(_context, R.drawable.spinner_layout3));
+        }
+        header_text.setText(headerTitle);
+
+        // If group is expanded then change the text into bold and change the
+        // icon
+
+        Drawable up = ContextCompat.getDrawable(_context, R.drawable.up_arrow_32);
+        Drawable down = ContextCompat.getDrawable(_context, R.drawable.down_arrow);
+
+        up.setVisible(false, false);
+        if (isExpanded) {
+            header_text.setTypeface(null, Typeface.BOLD);
+            //header_text.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.up_arrow, 0);
+            up.setVisible(true, false);
+        } else {
+            // If group is not expanded then change the text back into normal
+            // and change the icon
+
+            header_text.setTypeface(null, Typeface.NORMAL);
+            //header_text.setCompoundDrawablesWithIntrinsicBounds(0, 0,R.drawable.down_arrow, 0);
+            down.setVisible(true, true);
+        }
+
+        return convertView;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
+
+/*import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CustomAdapter extends ArrayAdapter<String> {
+
+    int[] spinnerIcons;
+    String[] spinnerNames;
+    String[] spinnerTimes;
+    String[] spinnerPrices;
+    Context mContext;
+
+    public CustomAdapter(@NonNull Context context, String[] serviceNames, int[] serviceIcons, String[] serviceTimes, String[] servicePrices) {
+        super(context, R.layout.custom_spinner_layout);
+        this.spinnerIcons = serviceIcons;
+        this.spinnerNames = serviceNames;
+        this.spinnerTimes = serviceTimes;
+        this.spinnerPrices = servicePrices;
+        this.mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return spinnerNames.length;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        ViewHolder mViewHolder = new ViewHolder();
+        if (convertView == null) {
+            LayoutInflater mInflater = (LayoutInflater) mContext.
+                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = mInflater.inflate(R.layout.custom_spinner_layout, parent, false);
+            mViewHolder.mIcon = (ImageView) convertView.findViewById(R.id.serviceIcon);
+            mViewHolder.mName = (TextView) convertView.findViewById(R.id.serviceName);
+            mViewHolder.mTime = (TextView) convertView.findViewById(R.id.waitingTime);
+            mViewHolder.mPrice = (TextView) convertView.findViewById(R.id.priceEstimate);
+            convertView.setTag(mViewHolder);
+        } else {
+            mViewHolder = (ViewHolder) convertView.getTag();
+        }
+        mViewHolder.mIcon.setImageResource(spinnerIcons[position]);
+        mViewHolder.mName.setText(spinnerNames[position]);
+        mViewHolder.mTime.setText(spinnerTimes[position]);
+        mViewHolder.mPrice.setText(spinnerPrices[position]);
+
+
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        ImageView mIcon;
+        TextView mName;
+        TextView mTime;
+        TextView mPrice;
+    }
+
+}*/
--- app/src/main/java/com/example/borja/finalproject/ServicesActivity.java
+++ app/src/main/java/com/example/borja/finalproject/ServicesActivity.java
@@ -1,40 +1,163 @@
 package com.example.borja.finalproject;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.Spinner;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.Toast;
 
-public class ServicesActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ServicesActivity extends Activity {
+    @SuppressLint("StaticFieldLeak")
+    private static ExpandableListView expandableListView;
+    @SuppressLint("StaticFieldLeak")
+    private static ExpandableListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_services);
 
-        String[] spinnerNames;
-        String[] emptyName = new String[]{""};;
-        String[] spinnerTimes;
-        String[] emptyTime = new String[]{""};
-        String[] spinnerPrices;
-        String[] emptyPrice = new String[]{""};
-        int[] spinnerIcons;
-        int[] emptyIcon = new int[]{R.drawable.empty};
-        Spinner mSpinner;
-        boolean isUserInteracting = false;
+        expandableListView = (ExpandableListView) findViewById(R.id.elistview);
 
-        mSpinner = (Spinner) findViewById(R.id.spinner);
+        // Setting group indicator null for custom indicator
+        expandableListView.setGroupIndicator(null);
 
-        spinnerNames = new String[]{"Uber", "Lyft", "Via"};
-        spinnerTimes = new String[]{"8 - 10 minutes", "8 - 10 minutes", "8 - 10 minutes"};
-        spinnerPrices = new String[]{"$7 - $10",  "$6 - $10,5", "$5 - $9,6"};
-        spinnerIcons = new int[]{R.drawable.uber_32
-                , R.drawable.lyft_32
-                , R.drawable.via_32 };
+        setItems();
+        setListener();
 
-        CustomAdapter mCustomAdapter = new CustomAdapter(ServicesActivity.this, spinnerNames, spinnerIcons, spinnerTimes, spinnerPrices);
-        mSpinner.setAdapter(mCustomAdapter);
+    }
 
-        //mSpinner.setAdapter(new CustomAdapter(ServicesActivity.this, emptyName, emptyIcon, emptyTime, emptyPrice));
+    // Setting headers and childs to expandable listview
+    void setItems() {
 
+        // Array list for header
+        ArrayList<String> header = new ArrayList<String>();
+
+        // Array list for child items
+        List<Service> child1 = new ArrayList<Service>();
+        List<Service> child2 = new ArrayList<Service>();
+        List<Service> child3 = new ArrayList<Service>();
+        List<Service> child4 = new ArrayList<Service>();
+
+
+        // Hash map for both header and child
+        HashMap<String, List<Service>> hashMap = new HashMap<String, List<Service>>();
+
+        // Adding headers to list
+
+        header.add("Ride Sharing");
+        header.add("Car Sharing");
+        header.add("Bike Sharing");
+        header.add("Walking");
+
+        // Adding child data
+        String serviceName = "Uber";
+        int serviceLogo = R.drawable.uber_32;
+        String serviceTime = "8 - 10 minutes";
+        String servicePrice = "$8 - $11";
+        Service sv = new Service(serviceName,serviceLogo,serviceTime,servicePrice);
+
+        String sN1 = "Lyft";
+        int sL1 = R.drawable.lyft_32;
+        String sT1 = "8 - 10 minutes";
+        String sP1 = "$8 - $11";
+        Service sv1 = new Service(sN1,sL1,sT1,sP1);
+
+        String sN2 = "Lyft";
+        int sL2 = R.drawable.via_32;
+        String sT2 = "8 - 10 minutes";
+        String sP2 = "$8 - $11";
+        Service sv2 = new Service(sN2,sL2,sT2,sP2);
+
+        child1.add(sv);
+        child1.add(sv1);
+        child1.add(sv2);
+
+        // Adding child data
+        for (int i = 1; i < 5; i++) {
+            //child2.add("Group 2  - " + " : Child" + i);
+
+        }
+        // Adding child data
+        for (int i = 1; i < 6; i++) {
+            //child3.add("Group 3  - " + " : Child" + i);
+
+        }
+        // Adding child data
+        for (int i = 1; i < 7; i++) {
+            //child4.add("Group 4  - " + " : Child" + i);
+
+        }
+
+        // Adding header and childs to hash map
+        hashMap.put(header.get(0), child1);
+        hashMap.put(header.get(1), child2);
+        hashMap.put(header.get(2), child3);
+        hashMap.put(header.get(3), child4);
+
+
+        adapter = new ExpandableListAdapter(ServicesActivity.this, header, hashMap);
+
+        // Setting adpater over expandablelistview
+        expandableListView.setAdapter(adapter);
+    }
+
+    // Setting different listeners to expandablelistview
+    void setListener() {
+
+        // This listener will show toast on group click
+        expandableListView.setOnGroupClickListener(new OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView listview, View view,
+                                        int group_pos, long id) {
+
+                Toast.makeText(ServicesActivity.this,
+                        "You clicked : " + adapter.getGroup(group_pos),
+                        Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        // This listener will expand one group at one time
+        // You can remove this listener for expanding all groups
+        expandableListView
+                .setOnGroupExpandListener(new OnGroupExpandListener() {
+
+                    // Default position
+                    int previousGroup = -1;
+
+                    @Override
+                    public void onGroupExpand(int groupPosition) {
+                        if (groupPosition != previousGroup)
+
+                            // Collapse the expanded group
+                            expandableListView.collapseGroup(previousGroup);
+                        previousGroup = groupPosition;
+                    }
+
+                });
+
+        // This listener will show toast on child click
+        expandableListView.setOnChildClickListener(new OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView listview, View view,
+                                        int groupPos, int childPos, long id) {
+                Toast.makeText(
+                        ServicesActivity.this,
+                        "You clicked : " + adapter.getChild(groupPos, childPos),
+                        Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
     }
 }
